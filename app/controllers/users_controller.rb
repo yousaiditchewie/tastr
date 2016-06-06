251eb4b1cdea33944bf2b11156acb9ad3cb571e3
Jacob Allen <jedijake@me.com>
2016-06-06 16:07:23 -0700

    make user automatically log in after signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "You have successfully signed up!"
       redirect_to root_path
     else
