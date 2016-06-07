86b84905bdfd18e1c78f38e932bcb8fc14c3305e
Jacob Allen <jedijake@me.com>
2016-06-07 16:39:12 -0700

    make /recipes/new and recipes/:id views

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,33 @@
 class RecipesController < ApplicationController
-  def new
-
+  def index
+    @recipes = Recipe.all
+  end
+  def show
+    @recipe = Recipe.find(params[:id])
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new recipe_params
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      render :new
+    end
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :recipe_name,
+      :cooking_instruction,
+      :photo_url,
+      :description,
+      :prep_time,
+      :cook_time,
+      :total_time
+  )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'recipes#index'
+  resources :recipes
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
