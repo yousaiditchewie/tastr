4202da7ed5a567c515376d2e0edcb47b36d5bdde
Jacob Allen <jedijake@me.com>
2016-06-09 10:05:04 -0700

    make edit feature for user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+
+  end
+
   private
     # Implement Strong Params
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'recipes#index'
   resources :recipes
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   get '/login', to: 'sessions#new'
   delete '/logout', to: 'sessions#destroy'
